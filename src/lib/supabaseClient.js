852f2c5a2dd55f2c5f7567d50cee7e7f33b29dd7
Maxlagartilloia <maxlagartilloia@gmail.com>
2026-02-01 14:10:21 -0500

    Create supabaseClient.js

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("ERROR: Variables de entorno de Supabase no detectadas.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
